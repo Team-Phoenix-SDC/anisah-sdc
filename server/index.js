b0e1a8f769c6ff9d0307e8266bbba160440233c2
Anisah Majeed <anisahmajeed4@gmail.com>
2022-03-11 15:07:27 -0500

    endpoints built out

--- server/index.js
+++ server/index.js
@@ -12,10 +12,30 @@ app.get('/', (req, res) => {
   res.send('meowwwww')
 });
 
+// endpoint for ALL products
+  // GET /products
 app.get('/products', async (req, res) => {
-  let result = await db.query('SELECT * FROM products WHERE id = 2');
-  res.send(result);
+  let data = await db.query('SELECT * FROM products')
+  res.status(200).send(data);
 })
+
+// endpoint for SINGULAR product
+  // GET /products/:product_id
+  // B E L O W  T H I S IS CURRENTLY A SAMPLE ???
+app.get('/products/:product_id', async (req, res) => {
+  let data = await db.query(`SELECT * FROM products WHERE id = 4`)
+  // i definitely need to change this query to be 'dynamic'
+  res.status(200).send(data);
+})
+
+// endpoint for ALL styles of a SINGULAR product
+  // GET /products/:product_id/styles
+app.get('/products/:product_id/styles', async (req, res) => {
+  let data = await db.query('SELECT * FROM styles');
+  console.log('yes?');
+  res.status(200).send(data);
+})
+//
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
