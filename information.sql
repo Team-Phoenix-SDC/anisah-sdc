de154306cd4c867bab80dab41ba6f98112624627
Anisah Majeed <anisahmajeed4@gmail.com>
2022-03-21 12:32:05 -0400

    changed name

--- information.sql
+++ information.sql
@@ -56,31 +56,3 @@ COPY photos
 FROM '/Users/anisahmajeed/spite/anisah-sdc/csv files/photos.csv'
 DELIMITER ','
 CSV HEADER;
-
--- CREATE TABLE products (
---   id integer NOT NULL,
---   name text NOT NULL,
---   slogan text NOT NULL,
---   description text NOT NULL,
---   category text NOT NULL,
---   default_price integer NOT NULL,
---   PRIMARY KEY(id, default_price)
--- );
-
--- CREATE TABLE styles (
---   style_id integer NOT NULL,
---   product_id integer NOT NULL,
---   "name" text,
---   original_price integer,
---   sale_price integer,
---   products_default_price integer,
---   PRIMARY KEY(style_id)
--- );
-
--- CREATE TABLE photos (
---   id integer NOT NULL,
---   styles_product_id integer NOT NULL,
---   url text,
---   thumbnail_url text,
---   PRIMARY KEY(id)
--- );
